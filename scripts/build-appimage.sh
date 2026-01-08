7c63eba3a595f952a63f2aee10af4f43f09ae08b
Maurício Szabo <mauricio.szabo@gmail.com>
2026-01-08 02:41:58 -0300

    Fixed some stuff, hopefully

--- scripts/build-appimage.sh
+++ scripts/build-appimage.sh
@@ -3,7 +3,6 @@ set -e
 
 # Download linuxdeploy and Qt plugin
 wget -q https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
-wget -q https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
 chmod +x linuxdeploy*.AppImage
 
 # Copy icon with the name expected by the desktop file
@@ -11,10 +10,12 @@ cp packaging/unix/midieditor/logo48.png packaging/unix/midieditor/midieditor.png
 mv MidiEditor midieditor
 
 # Set up environment for linuxdeploy
-export LINUXDEPLOY_OUTPUT_VERSION=$(git describe --tags --always)
+#export LINUXDEPLOY_OUTPUT_VERSION=$(git describe --tags --always)
 export QMAKE=/usr/bin/qmake6
 export QML_SOURCES_PATHS=.
-export EXTRA_QT_PLUGINS="multimedia"
+export QT_QPA_PLATFORM=xcb
+export EXTRA_QT_PLUGINS="multimedia;wayland"
+export NO_STRIP=true
 
 # Create AppImage
 ./linuxdeploy-x86_64.AppImage \
@@ -22,9 +23,6 @@ export EXTRA_QT_PLUGINS="multimedia"
   --executable midieditor \
   --desktop-file packaging/unix/midieditor/MidiEditor.desktop \
   --icon-file packaging/unix/midieditor/midieditor.png \
-  --library /usr/lib/x86_64-linux-gnu/libfluidsynth.so.3 \
-  --library /usr/lib/x86_64-linux-gnu/libasound.so.2 \
-  --plugin qt \
   --output appimage
 
 # Rename AppImage to a more user-friendly name
